f84d78c7e842b444dd8578213daffdb341051077
gedo19 <eu.MaD19@yandex.ru>
2022-02-05 20:44:16 +0600

    linter fixes

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import pageLoader from "../src/pageLoader.js";
+import pageLoader from '../src/pageLoader.js';
 
 const program = new Command();
 
@@ -10,7 +10,7 @@ program
   .argument('<url>')
   .description('Page loader utility')
   .version('0.0.1')
-  .option('-o, --output [dir]', `output dir`, process.cwd())
+  .option('-o, --output [dir]', 'output dir', process.cwd())
   .action((url) => {
     pageLoader(url, program.opts().output).then(console.log);
   });
