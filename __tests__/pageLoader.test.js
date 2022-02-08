5046cfa320c458f2d15efed063ca64915a0f536b
gedo19 <eu.MaD19@yandex.ru>
2022-02-08 20:18:58 +0600

    Stage 2

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -21,12 +21,39 @@ beforeAll(async () => {
   html = await fs.readFile(getFixturePath('example.html'), 'utf-8');
 });
 
-test('pageLoader', async () => {
+test('pageLoader should save html file', async () => {
   nock(/ru\.hexlet\.io/)
     .get(/courses/)
     .reply(200, html);
 
   const filepath = await pageLoader('https://ru.hexlet.io/courses', dir);
   const content = await fs.readFile(filepath, 'utf-8');
+
   expect(content).toBe(html);
+});
+
+test('when the http request fails', async () => {
+  nock(/wrong\.url\.wrong/)
+    .get(/what/)
+    .replyWithError('Wrong url!');
+  expect.assertions(1);
+
+  await expect(pageLoader('https://wrong.url.wrong/what', dir)).rejects.toThrow('Wrong url!');
+});
+
+test('pageLoader should save images', async () => {
+  const beforeHtml = await fs.readFile(getFixturePath('htmlWithImg_before.html'), 'utf-8');
+  const img = await fs.readFile(getFixturePath('node_js.png'));
+  nock(/ru\.hexlet\.io/)
+    .get(/courses/)
+    .reply(200, beforeHtml);
+  nock(/ru\.hexlet\.io/)
+    .get(/assets\/professions\/nodejs.png/)
+    .reply(200, img);
+
+  const afterHtml = await fs.readFile(getFixturePath('htmlWithImg_after.html'), 'utf-8');
+  const filepath = await pageLoader('https://ru.hexlet.io/courses', dir);
+  const content = await fs.readFile(filepath, 'utf-8');
+
+  expect(content).toBe(afterHtml);
 });
--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,19 +1,75 @@
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 import fs from 'fs/promises';
 import * as path from 'path';
+import prettier from 'prettier';
 
-const buildFilename = (url) => {
-  const urlWithoutProtocol = url.replace(/https?:\/\//, '');
-  const filename = urlWithoutProtocol.replace(/[^a-z0-9]/gi, '-');
-  return `${filename}.html`;
+const getNameByUrl = (url) => {
+  const { protocol } = url;
+  const regex = new RegExp(`${protocol}//`);
+  const urlWithoutProtocol = url.href.replace(regex, '');
+  return urlWithoutProtocol
+    .replace(/\/$/, '')
+    .replace(/[^a-z0-9]/gi, '-');
 };
 
-export default (url, dir) => axios.get(url)
-  .catch((error) => {
-    throw new Error(`${error.message}!`);
+const getFilepath = {
+  files: (outputPath, url) => {
+    const filepath = path.join(outputPath, getNameByUrl(url));
+    return `${filepath}_files`;
+  },
+  img: (outputPath, url) => {
+    const { dir, ext, name } = path.parse(url.href);
+    const urlWithoutExtname = new URL(path.join(dir, name));
+    const filepath = path.join(outputPath, getNameByUrl(urlWithoutExtname));
+    return `${filepath}${ext || '.png'}`;
+  },
+  html: (outputPath, url) => {
+    const filepath = path.join(outputPath, getNameByUrl(url));
+    return `${filepath}.html`;
+  },
+};
+
+const vars = {};
+
+export default (url, outputPath) => axios.get(url)
+  .catch((e) => {
+    throw new Error(e.message);
   })
-  .then((response) => {
-    const filepath = path.join(dir, buildFilename(url));
-    const { data } = response;
-    return fs.writeFile(filepath, data).then(() => filepath);
+  .then(({ data }) => {
+    vars.url = new URL(url);
+    vars.data = data;
+    vars.filesDirpath = getFilepath.files(outputPath, vars.url);
+    vars.filesRelDirpath = getFilepath.files('', vars.url);
+
+    return fs.mkdir(vars.filesDirpath);
+  })
+  .then(() => {
+    const promises = [];
+    const $ = cheerio.load(vars.data);
+    $('img')
+      // eslint-disable-next-line func-names
+      .each(function () {
+        const src = $(this).attr('src');
+        const imgUrl = new URL(src, vars.url);
+        if (imgUrl.host === vars.url.host) {
+          const filepath = getFilepath.img(vars.filesDirpath, imgUrl);
+          const relFilepath = getFilepath.img(vars.filesRelDirpath, imgUrl);
+
+          const promise = axios({
+            method: 'get',
+            url: imgUrl.href,
+            responseType: 'arraybuffer',
+          }).then(({ data }) => {
+            $(this).attr('src', relFilepath);
+            return fs.writeFile(filepath, data);
+          });
+          promises.push(promise);
+        }
+      });
+    return Promise.all(promises).then(() => $.html());
+  })
+  .then((html) => {
+    const filepath = getFilepath.html(outputPath, vars.url);
+    return fs.writeFile(filepath, prettier.format(html, { parser: 'html' })).then(() => filepath);
   });
